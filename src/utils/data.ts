53487bef89371ff1629e5ee1b05f783af81b8c57
OverLate <noam.richard@epitech.eu>
2023-12-22 16:22:45 +0100

    initial commit

---
+++ src/utils/data.ts
@@ -0,0 +1,40 @@
+const services = [
+    {
+        title: "Système de Points de Fidélité",
+        description: "Développé en C# et PostgreSQL, le système de points de fidélité visait à récompenser les clients avec des points de fidélité pour chaque achat effectué. Le système gérait et suivait efficacement les points de fidélité des clients, améliorant ainsi l'expérience globale du client.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Système de Gestion Pharmaceutique",
+        description: "Ce projet consistait à créer un système de gestion pharmaceutique pour aider les professionnels de la santé dans leurs interactions avec les médecins. Le système facilitait la vente de médicaments, offrant une solution complète pour gérer les activités de vente de produits pharmaceutiques.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Portfolio ReactJS",
+        description: "Dans le cadre de ma troisième année à Epitech, j'ai créé un portfolio en utilisant ReactJS. Le portfolio présentait mes compétences, projets et réalisations, offrant une représentation dynamique et engageante de mon travail.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Jeu de Combat des Chevaliers en Java",
+        description: "En collaboration avec une équipe de trois personnes, nous avons développé un jeu de combat en 2D en Java. Le jeu mettait en scène des chevaliers engagés dans des batailles passionnantes, démontrant notre capacité à créer une expérience de jeu interactive et divertissante.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Application de Recherche d'Emploi",
+        description: "Conception d'une application de recherche d'emploi permettant aux utilisateurs de publier des offres d'emploi ou de postuler à des postes disponibles. La plateforme facilitait le processus efficace de correspondance et d'application pour des emplois, contribuant à une expérience de recherche d'emploi rationalisée.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Sécurité Réseau pour Parc d'Attractions",
+        description: "Mise en place d'une infrastructure réseau pour un parc d'attractions, intégrant plusieurs PC et machines virtuelles. Le projet mettait l'accent sur la sécurisation des connexions et la protection contre les menaces de cybersécurité. La conception du réseau visait à garantir un environnement sûr et fiable pour les opérations du parc.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+];
+
+export { services };
